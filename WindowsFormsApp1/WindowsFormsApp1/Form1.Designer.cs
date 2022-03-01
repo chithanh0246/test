dff3f026321553ea629ba1fe967ad28ad370e4ca
HoaiPhong <vhphong2000@gmail.com>
2022-02-28 19:18:46 -0800

    Phong

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -77,9 +77,9 @@ namespace WindowsFormsApp1
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(288, 144);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(89, 16);
+            this.label1.Size = new System.Drawing.Size(87, 16);
             this.label1.TabIndex = 5;
-            this.label1.Text = "UserNameT1";
+            this.label1.Text = "UsernameLG";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
